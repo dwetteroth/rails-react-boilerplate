74387b01769e692eaed42e5ff2b6ec80abf21776
Alyssa Lundgren <lundgrea@gmail.com>
2020-07-06 07:55:16 -0600

    Finish project drop down

--- front/js/components/ProjectDropDown/ProjectDropDown.js
+++ front/js/components/ProjectDropDown/ProjectDropDown.js
@@ -8,7 +8,7 @@ import React from 'react';
 const ProjectDropDown = () => {
   return (
     <div>
-        <button >project 1</button>
+        <button>project 1</button>
         <button>project 2</button>
     </div>
   );
