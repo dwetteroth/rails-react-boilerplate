92cd40dbbfef995e4b4b6cfecefb156cb1122beb
Alyssa Lundgren <lundgrea@gmail.com>
2020-07-08 09:33:37 -0600

    Add Broswer Router to app.js file and wrap App in Browser Router

--- front/js/components/app.js
+++ front/js/components/app.js
@@ -6,6 +6,7 @@ import '@babel/polyfill';
 
 import React from 'react';
 import ReactDom from 'react-dom';
+import BrowserRouter from 'react-router-dom'
 
 // Include the main scss file for webpack processing.
 import '../css/app.scss';
@@ -15,10 +16,16 @@ import getLogger from './utils/logger';
 
 const log = getLogger('App');
 
+const router = (
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+)
+
 const init = () => {
   log.info('init() :: App starts booting...');
 
-  ReactDom.render(<App />, document.getElementById('app'));
+  ReactDom.render(router, document.getElementById('app'));
 };
 
 // init app
