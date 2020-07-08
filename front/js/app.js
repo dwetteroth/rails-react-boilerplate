dfda8a34cd78c55d54dc4feb17f231bf1f90a80b
Alyssa Lundgren <lundgrea@gmail.com>
2020-07-08 17:10:56 -0600

    Fix location of app.js file as App Entry

--- front/js/app.js
+++ front/js/app.js
@@ -11,21 +11,21 @@ import BrowserRouter from 'react-router-dom'
 // Include the main scss file for webpack processing.
 import '../css/app.scss';
 
-import App from './components/App/App';
+import App from './components/App/App'
 import getLogger from './utils/logger';
 
 const log = getLogger('App');
 
-const router = (
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
-)
+// const router = (
+//   <BrowserRouter>
+//     <App/>
+//   </BrowserRouter>
+// )
 
 const init = () => {
   log.info('init() :: App starts booting...');
 
-  ReactDom.render(router, document.getElementById('app'));
+  ReactDom.render(<App />, document.getElementById('app'));
 };
 
 // init app
