94eb01dc5af2e99cdc46c7057b9d47b857a0a101
Noah <noah@centilgroup.com>
2020-07-07 18:25:27 -0600

    created nav and dashboard components

---
+++ front/js/components/Nav/Nav.js
@@ -0,0 +1,18 @@
+
+
+import React from 'react';
+
+const Nav = () => { 
+    return (
+    <Nav>
+    <img>IDB Logo</img>
+    <span>
+        <h3>projects</h3>
+        <h3>settings</h3>
+        <h3>logout</h3>
+    </span>
+    </Nav>
+    );
+};
+
+export default Nav 
