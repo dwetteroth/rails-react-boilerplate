b35c9aad369f39109568d3f02f30d52e455c55ec
Alyssa Lundgren <lundgrea@gmail.com>
2020-07-08 09:36:19 -0600

    Add Route and component Routes to App.js

--- front/js/components/App/App.js
+++ front/js/components/App/App.js
@@ -3,23 +3,33 @@
  */
 
 import React, { Component } from 'react';
+import Route from 'react-router-dom';
 
 export class App extends Component {
   constructor() {
     super()
     this.state = {
-    username: "",
-    email: "",
-    projects:[],
-    teammates: [],
-    isLoading: true,
-    networkMessage: '',
-    error: ''
-  };
-
+      username: "",
+      email: "",
+      projects:[],
+      teammates: [],
+      isLoading: true,
+      networkMessage: '',
+      error: ''
+    }
+  }
 
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <section>
+      <Route exact path='/' render={() => <Login loginUser={this.loginUser}/>}/>
+      <Route path='/home' component={() => <Main username={this.state.username}/>}/>
+      <Route path='/settings' component={() => <Settings username={this.state.username} email={this.state.email} teammates={this.state.teammates} projects={this.state.projects}/>}/>
+      <Route path='/project/:id' component={() => <Dashboard projectInfo={this.state.projects}/>}/>
+      <Route path='/project/:id/detail/:id' component={() => <FlowDetails/>}/>
+    </section>
+    )
   }
 }
+
 export default App;
