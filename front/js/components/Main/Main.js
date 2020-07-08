72afe0e5535ba13768b929e61b4868fc80634fc9
Alyssa Lundgren <lundgrea@gmail.com>
2020-07-08 09:43:41 -0600

    Add Main.js props.username to demonstrate passing props once logged in

--- front/js/components/Main/Main.js
+++ front/js/components/Main/Main.js
@@ -4,11 +4,12 @@
 
 import React from 'react';
 
-const Main = () => {
+const Main = (props) => {
   return (
-    <div>
-      <h1>I am the Main</h1>
-    </div>
+    <main>
+      <h1>This is the Main</h1>
+      <p>Welcome, {props.username}</p>
+    </main>
   );
 };
 
